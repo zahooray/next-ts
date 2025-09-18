abbe4c5bb49c1602e8bef18e15ea4607551cc246
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2025-09-18 10:38:23 +0500

    refactor: removed all console logs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   const greeting: string = "Hello, Next.js with Typescript!";
-  console.log(greeting);
   const isActive: boolean = true;
-  console.log(`Is Active: ${isActive}`);
   const count: number = 42;
-  console.log(`Count: ${count}`);
   const numbers: number[] = [1, 2, 3];
   const strings: string[] = ["a", "b"];
   const tuples: [number, string] = [1, "a"];
@@ -15,10 +12,8 @@ export default function Home() {
     { id: 1, values: ["x", "y"] },
     { id: 2, values: ["a", "b"] },
   ];
-  console.log(nestedArr);
   const func = (x: number): number => x ** 2;
 
-  console.log(numbers, strings, tuples, mixed, obj, func(5));
 
   const user: { id: number; name: string; email: string } = {
     id: 1,
@@ -26,14 +21,12 @@ export default function Home() {
     email: "Ahmad@ahmad.com",
   };
 
-  console.log(user);
   type User = { id: number; name: string; email: string };
   const anotherUser: User = {
     id: 2,
     name: "Kamran",
     email: "Kamran@kamrandev.com",
   };
-  console.log(anotherUser);
 
   type NestedUser = {
     id: number;
@@ -48,7 +41,6 @@ export default function Home() {
       { degree: "PhD", year: 2025 },
     ],
   };
-  console.log(nestedUser);
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
