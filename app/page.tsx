5cd32ab460251f1d8b8f484ec6c6c6ed95713222
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2025-09-18 10:35:40 +0500

    fix: practice basic types in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const greeting: string = "Hello, Next.js with Typescript!";
+  console.log(greeting);
+  const isActive: boolean = true;
+  console.log(`Is Active: ${isActive}`);
+  const count: number = 42;
+  console.log(`Count: ${count}`);
+  const numbers: number[] = [1, 2, 3];
+  const strings: string[] = ["a", "b"];
+  const tuples: [number, string] = [1, "a"];
+  const mixed: (number | string)[] = [1, "b", 2, "c"];
+  const obj: { id: number; name: string } = { id: 1, name: "John" };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const nestedArr: { id: number; values: string[] }[] = [
+    { id: 1, values: ["x", "y"] },
+    { id: 2, values: ["a", "b"] },
+  ];
+  console.log(nestedArr);
+  const func = (x: number): number => x ** 2;
+
+  console.log(numbers, strings, tuples, mixed, obj, func(5));
+
+  const user: { id: number; name: string; email: string } = {
+    id: 1,
+    name: "Ahmad",
+    email: "Ahmad@ahmad.com",
+  };
+
+  console.log(user);
+  type User = { id: number; name: string; email: string };
+  const anotherUser: User = {
+    id: 2,
+    name: "Kamran",
+    email: "Kamran@kamrandev.com",
+  };
+  console.log(anotherUser);
+
+  type NestedUser = {
+    id: number;
+    profile: { name: string; age: number };
+    education?: { degree: string; year: number }[];
+  };
+  const nestedUser: NestedUser = {
+    id: 3,
+    profile: { name: "Hassan", age: 12 },
+    education: [
+      { degree: "Masters", year: 2020 },
+      { degree: "PhD", year: 2025 },
+    ],
+  };
+  console.log(nestedUser);
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        Hello, Next.js with Typescript!
+      </h1>
     </div>
   );
 }
